a999a6b54812d868719d30845c3003f14c25b22b
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-04-25 09:43:20 -0300

    Execícios 2 : Playlist

---
+++ Ex2.js
@@ -0,0 +1,7 @@
+let playlist = ["The monster - Eminem", "Hotel California - Eagles", "Construção - Chico Buarque", "Heavy is the crowd - Link Park"];
+
+let tamanho = playlist.length;
+
+for(let i = 0; i < tamanho; i ++){
+    console.log(playlist[i])
+}
